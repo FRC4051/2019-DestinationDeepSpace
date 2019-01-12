969c744d52422403e1d87581e848e02136968e89
nirornith <zoeraed712@gmail.com>
2019-01-12 13:01:40 -0800

    Zoe's Thing or Whatever
    zoe is best - Shireen
    
    UUGGGGHHHH- Zoe

---
+++ SpaceCase/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+  public static Victor driveMotor1 = new Victor(0);
+  public static Victor driveMotor2 = new Victor(1);
+}
